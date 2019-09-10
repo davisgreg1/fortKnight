384b10dd2cc457ceac1164c64a42c731d376ad97
Andrew Usher <andrewusher00@gmail.com>
2019-09-09 20:48:09 -0500

    Remove docs comments

--- server/index.js
+++ server/index.js
@@ -17,8 +17,6 @@ const typeDefs = gql`
   }
 `
 
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
     battleRoyaleNews: async (_source, _args, { dataSources }) => dataSources.FortniteAPI.getBattleRoyaleNews(),
