5bc5958ce25853b44e49c914fd08af33bc2da6f2
Gregory Davis <davisgreg1@gmail.com>
2019-09-14 13:04:13 -0500

    file restructure and css classnames

--- client/src/Components/NavBar/index.js
+++ client/src/Components/NavBar/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import "./styles.scss";
+import styles from "./styles.scss";
+
+const navBarItems = [
+  {
+    id: 1,
+    title: "Home",
+    to: "/",
+    className: "nav-link"
+  }, {
+    id: 2,
+    title: "Weapons",
+    to: "/weapons",
+    className: "nav-link"
+  }
+]
 
 export default function NavBar() {
   return (
-    <nav clasName="nav">
-      <Link to="/" className="nav-link">Home</Link>
-      <Link to="/weapons" className="nav-link">Weapons</Link>
+    <nav clasName={styles.nav}>
+      {navBarItems.map(item => {
+        return (
+          <Link to={item.to} className={styles[item.className]}>{item.title}</Link>
+        )
+      })
+}
     </nav>
   )
 }
